855568df0ebb4d7dfa4b260c1ec72c228b9f0601
Ruben <rubenabelyan.bangits@gmail.com>
2021-10-21 15:27:47 +0400

    added logOut domain Layer

---
+++ src/adapter/redux/actions/logOut.ts
@@ -0,0 +1,13 @@
+
+export const logoutTypes = {
+    LOG_OUT: `LOG_OUT`,
+} as const;
+
+export const logoutActions = {
+  loginRequest: (logOutRequestModel) => ({
+    type: logoutTypes.LOG_OUT,
+    payload: logOutRequestModel
+  })
+};
+
+// export type LogOutActions = ReturnType<InferValueTypes<typeof >>;
---
+++ src/adapter/redux/sagas/logOut.ts
@@ -0,0 +1,13 @@
+import { fork, takeLatest } from 'redux-saga/effects';
+import { logoutActions, logoutTypes } from '../actions';
+
+function* logOut(action: ReturnType<typeof logoutActions.loginRequest>) {
+}
+
+function* watchLogOutRequest() {
+    yield takeLatest(logoutTypes.LOG_OUT, logOut);
+  }
+
+const logOutSagas = [fork(watchLogOutRequest)];
+
+export default logOutSagas;
