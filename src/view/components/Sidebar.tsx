310d8b5899c4009848cb3c74fc7a87c88937f8a0
Armen Mesropyan <amesropyan@bangits.com>
2022-02-24 17:46:43 +0400

    Removed historyService

--- src/view/components/Sidebar.tsx
+++ src/view/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { historyService, useLocation, useTranslation } from '@atom/common';
+import { redirectToURL, useLocation, useTranslation } from '@atom/common';
 import { Icons, Sidebar as DesignSystemSidebar } from '@atom/design-system';
 import { useCallback } from 'react';
 import { Logo } from '../images';
@@ -9,7 +9,8 @@ export const Sidebar = () => {
 
   const createRedirectHandler = useCallback(
     (url: string) => () => {
-      historyService.redirectToURL(url);
+      // historyService.redirectToURL(url);
+      redirectToURL(url);
     },
     []
   );
