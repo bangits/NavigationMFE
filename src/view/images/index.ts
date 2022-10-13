c7d1f16765437a3f700de0efaf7259db8ed4fd71
Armen Mesropyan <amesropyan@bangits.com>
2022-10-13 19:45:13 +0400

    Add bet rabbit project

--- src/view/images/index.ts
+++ src/view/images/index.ts
@@ -2,4 +2,6 @@ export const Logo = 'https://storageaccountatom.blob.core.windows.net/mfe/logo.s
 export const BangitsLogo = 'https://storageaccountatom.blob.core.windows.net/mfe/bangits.svg';
 export const KingBetLogo =
   'https://storageaccountatom.blob.core.windows.net/storage/Iconimages/icon/0e69645c-9957-4f06-9e0f-83ba95e67dca_KINGBET.svg';
+export const BetRabbitLogo =
+  'https://storageaccountatom.blob.core.windows.net/storage/BetRabbit-Logo/22ea8c22-1141-4f8b-80c6-d30790197f6b_Group%204722.svg';
 export const QantoApuestasLogo = 'https://storageaccountatom.blob.core.windows.net/mfe/quanto-apuestas.svg';
