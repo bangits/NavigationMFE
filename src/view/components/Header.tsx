7290f62cfd1da4580c095b97a8f366db336b104e
Armen Mesropyan <amesropyan@bangits.com>
2022-12-27 15:50:24 +0400

    Fix

--- src/view/components/Header.tsx
+++ src/view/components/Header.tsx
@@ -24,7 +24,6 @@ export const Header: FC<HeaderProps> = ({ onLogOut, username, money, currency })
   const correctBalanceLabel = isBetShopUser(user) ? t.get('cashInOut') : t.get('correctBalance');
 
   useEffect(() => {
-    // @ts-expect-error Disable TS for type cast navigator
     if (navigator?.connection) navigator.connection.addEventListener('change', () => setWifiSpeed(calculateWifiSpeed));
 
     window.addEventListener('online', () => setOnline(true));
